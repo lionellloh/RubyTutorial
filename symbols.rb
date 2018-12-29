de45231e9eac4087554e14978c36ae32dfd5a8e4
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2018-12-29 10:42:07 +0800

    Some Simple Ruby Data Structures

---
+++ symbols.rb
@@ -0,0 +1,15 @@
+class Symbols
+
+person = {
+    first_name: "Benjamin",
+    last_name: "Frank"
+}
+
+  inclusive = 1..10
+  exclusive = 1...10
+  array = [*inclusive]
+
+  # As long as first letter is upper case, Ruby recognises it as a constant
+  MAX_SCORE = 100
+end
+
